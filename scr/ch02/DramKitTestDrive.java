4b37e97f39a120feedaef2272a05152f94eaa764
pollyshenko <pollyshenko@gmail.com>
2024-05-23 15:06:03 +0400

    commit 4

--- scr/ch02/DramKitTestDrive.java
+++ scr/ch02/DramKitTestDrive.java
@@ -2,9 +2,18 @@ package ch02;
 
 public class DramKitTestDrive {
     public static void main(String[] args){
+
         DrumKit d = new DrumKit();
+
         d.playSnare();
+
         d.snare = false;
+
+        if(d.snare == true){
+            d.playSnare();
+        }
+
+        d.playTopHat();
 
     }
 }
--- scr/ch02/DrumKit.java
+++ scr/ch02/DrumKit.java
@@ -1,11 +1,11 @@
 package ch02;
 
 public class DrumKit {
+    boolean topHat = true;
+    boolean snare = true;
     public void playSnare(){
         System.out.println("бах бах ба-бах");
     }
-    boolean topHat = true;
-    boolean snare = true;
 
     void playTopHat(){
         System.out.println("динь динь ди-динь");
