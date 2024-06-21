44c5d964032f2a48e9527721705912f46f7dbdb6
pollyshenko <pollyshenko@gmail.com>
2024-06-21 14:47:39 +0400

    commit:  calcArea

---
+++ scr/ch04/calcAreaM.java
@@ -0,0 +1,21 @@
+package ch04;
+
+public class calcAreaM {
+
+    public int calcArea(int h, int w){
+        return h * w;
+    }
+
+    int a = calcArea(7,12);
+    short c = 7;
+    calcArea(2,3);
+    int d = calcArea(57);
+    long t = 42;
+    int f = calcArea(t,17);
+    int g = calcArea();
+    calcArea();
+    byte h = calcArea(4,20);
+    int j = calcArea(2,3,5);
+
+
+}
